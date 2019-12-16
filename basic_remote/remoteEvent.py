10ba8d9d58fc92a5830515c71e926c697027aa04
xaaradise96 <alisonshanna.ah@gmail.com>
2019-12-16 16:02:22 +0100

    suite

---
+++ basic_remote/remoteEvent.py
@@ -0,0 +1,60 @@
+import click
+import spytank
+
+z  = "z : avancer "
+s  = "s : reculer"
+q  = "q : tourner a gauche"
+d  = "d : tourner a droite"
+vp = "+ : plus vite"
+vm = "- : moins vite"
+x  = "x : stop "
+vitesse = 150
+direction = ""
+
+
+
+print(z,s,q,d,vp,vm,x)
+
+while True :
+    lettre = click.getchar()
+    if lettre =="z":
+        spytank.avance(vitesse)
+        direction = 1
+    elif lettre =="s":
+        spytank.recule(vitesse)
+        direction = 2
+    elif lettre == "q":
+        spytank.gauche(vitesse) 
+        direction = 3 
+    elif lettre == "d":
+        spytank.droite(vitesse)
+        direction = 4
+    elif lettre == "+":
+        if vitesse < 250 :
+            vitesse = vitesse +10
+
+        if direction == 1:
+            spytank.avance(vitesse)
+        elif direction == 2:
+            spytank.recule(vitesse)
+        elif direction == 3:
+            spytank.gauche(vitesse)
+        elif direction == 4:
+            spytank.droite(vitesse) 
+
+    elif lettre == "-":
+        if vitesse > 70 :
+            vitesse = vitesse -10
+
+        if direction == 1:
+            spytank.avance(vitesse)
+        elif direction == 2:
+            spytank.recule(vitesse)
+        elif direction == 3:
+            spytank.gauche(vitesse)
+        elif direction == 4:
+            spytank.droite(vitesse) 
+
+    elif  lettre=="x":
+        spytank.stop()
+    print("tu as tapé la lettre:", lettre)
