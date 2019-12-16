f2f79f16201145cee6a7e40a9a68516b34c1505c
xaaradise96 <alisonshanna.ah@gmail.com>
2019-12-16 15:10:47 +0100

    suit

--- basic_remote/remotelavier.py
+++ basic_remote/remotelavier.py
@@ -1,11 +1,34 @@
-import spytank 
+import spytank
+
+z  = "z : avancer "
+s  = "s : reculer"
+q  = "q : tourner a gauche"
+d  = "d : tourner a droite"
+vp = "a : plus vite"
+vm = "e : moins vite"
+x  = "x : stop "
+vitesse = 150
+
+
+
+print(z,s,q,d,vp,vm,x)
 
 while True:
     lettre=input(">>>")
     if lettre =="z":
-        spytank.avance(150)
+        spytank.avance(vitesse)
     elif lettre =="s":
-        spytank.recule(150) 
+        spytank.recule(vitesse)
+    elif lettre == "q":
+        spytank.gauche(vitesse)  
+    elif lettre == "d":
+        spytank.droite(vitesse)
+    elif lettre == "a":
+        if vitesse < 250 :
+            vitesse = vitesse +10
+    elif lettre == "e":
+        if vitesse > 70 :
+            vitesse = vitesse -10
     elif  lettre=="x":
         spytank.stop()      
  
